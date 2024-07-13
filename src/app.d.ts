10a50e436ec219b02a3bd267d9b29e47aebcc1bf
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-07-13 17:48:33 +0700

    added cloudflare stuff

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,7 +6,12 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
+
+		interface Platform {
+			env: Env;
+			cf: CfProperties;
+			ctx: ExecutionContext;
+		}
 	}
 }
 
