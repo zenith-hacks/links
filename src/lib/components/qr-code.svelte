8dae7f9c87829c56781e4518481a2118bb38e5f2
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-07-14 00:20:58 +0700

    working qr code dialog

--- src/lib/components/qr-code.svelte
+++ src/lib/components/qr-code.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
-	import { QrCode } from 'lucide-svelte';
+	import QRCode from 'qrcode';
+	import { QrCodeIcon } from 'lucide-svelte';
 	import { Button, buttonVariants } from '$lib/components/ui/button';
 	import * as Dialog from '$lib/components/ui/dialog';
 
 	export let url: string;
+
+	let qrCodeData = '';
+	$: QRCode.toDataURL(url, { margin: 2, width: 512 }).then((data) => (qrCodeData = data));
 </script>
 
 <Dialog.Root>
 	<Dialog.Trigger class={buttonVariants({ size: 'icon', variant: 'secondary' })}>
-		<QrCode size={18} />
+		<QrCodeIcon size={18} />
 		<span class="sr-only">Show QR code</span>
 	</Dialog.Trigger>
 	<Dialog.Content class="sm:max-w-md">
@@ -18,5 +22,11 @@
 				This is the QR code for the shortened URL. You can scan it with your phone to visit the URL.
 			</Dialog.Description>
 		</Dialog.Header>
+		<div class="my-2 flex w-full justify-center">
+			<img src={qrCodeData} alt="QR code" class="aspect-square w-80" />
+		</div>
+		<Dialog.Footer>
+			<Button href={qrCodeData} download>Download</Button>
+		</Dialog.Footer>
 	</Dialog.Content>
 </Dialog.Root>
