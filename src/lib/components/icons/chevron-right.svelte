b03d92e4c3f92efcdfa0df059b65266d68113115
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-07-13 21:35:20 +0700

    added form and more stuff

---
+++ src/lib/components/icons/chevron-right.svelte
@@ -0,0 +1,16 @@
+<svg
+	fill="none"
+	stroke="currentColor"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	stroke-width="1.5"
+	viewBox="0 0 16 16"
+	class="text-orange"
+	{...$$restProps}
+>
+	<path d="M12 8H4" class="origin-center scale-x-0 transition-transform group-hover:scale-x-100" />
+	<path
+		d="m6.5 11.5 3.146-3.146a.5.5 0 0 0 0-.708L6.5 4.5"
+		class="transition-transform group-hover:translate-x-[2px]"
+	/>
+</svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,20 @@
-<main class="flex w-full flex-col items-center px-6 pt-20 md:pt-28 lg:pt-36">
+<script>
+	import ChevronRight from '$lib/components/icons/chevron-right.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+</script>
+
+<main class="flex w-full flex-col items-center space-y-6 px-6 pt-20 md:pt-28 lg:pt-36">
 	<h1 class="text-center text-5xl font-bold">The simplest URL shortener</h1>
+	<p class="max-w-lg text-center text-muted-foreground">
+		Shorten your long URLs into a shorter one with ease. Just paste your long URL below to shorten
+		it.
+	</p>
+	<form class="flex w-full justify-center space-x-2">
+		<Input class="max-w-lg" placeholder="Enter your URL" type="url" />
+		<Button class="group">
+			<span class="mr-1">Shorten</span>
+			<ChevronRight width={16} height={16} />
+		</Button>
+	</form>
 </main>
